f25d50efd5b601d0620c5a263964782f9e124a3c
GeekCoderJim <13850745359@163.com>
2018-01-15 15:50:33 +0800

    添加了goodstudent类

---
+++ meituan/meituan/goodStudent.h
@@ -0,0 +1,13 @@
+//
+//  goodStudent.h
+//  meituan
+//
+//  Created by 陈恒均 on 2018/1/15.
+//  Copyright © 2018年 陈恒均. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface goodStudent : NSObject
+
+@end
