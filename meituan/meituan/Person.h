cd34a547d809aee42a465f7eeef82581ea61a92c
GeekCoderJim <13850745359@163.com>
2018-01-15 15:47:42 +0800

    添加了person类

---
+++ meituan/meituan/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  meituan
+//
+//  Created by 陈恒均 on 2018/1/15.
+//  Copyright © 2018年 陈恒均. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ meituan/meituan/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  meituan
+//
+//  Created by 陈恒均 on 2018/1/15.
+//  Copyright © 2018年 陈恒均. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
