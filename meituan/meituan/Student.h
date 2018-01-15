d71fca9afd8e73b6a6c6b43656954cede99ed0b2
GeekCoderJim <13850745359@163.com>
2018-01-15 15:49:43 +0800

    添加了student类

--- meituan/meituan/Student.h
+++ meituan/meituan/Student.h
@@ -1,14 +1,13 @@
 //
-//  Person.m
+//  Student.h
 //  meituan
 //
 //  Created by 陈恒均 on 2018/1/15.
 //  Copyright © 2018年 陈恒均. All rights reserved.
 //
 
-#import "Person.h"
+#import <Foundation/Foundation.h>
 
-@implementation Person
+@interface Student : NSObject
 
 @end
-
--- meituan/meituan/Student.m
+++ meituan/meituan/Student.m
@@ -1,14 +1,13 @@
 //
-//  Person.m
+//  Student.m
 //  meituan
 //
 //  Created by 陈恒均 on 2018/1/15.
 //  Copyright © 2018年 陈恒均. All rights reserved.
 //
 
-#import "Person.h"
+#import "Student.h"
 
-@implementation Person
+@implementation Student
 
 @end
-
